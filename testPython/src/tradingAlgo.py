e34b56675a210f1243521a10905b1cbbb5a498ee
sumit-bhatkar <sumitbhatkar@gmail.com>
2020-07-31 08:01:53 +0530

    more tests

--- testPython/src/tradingAlgo.py
+++ testPython/src/tradingAlgo.py
@@ -1,15 +1,19 @@
 import csv
 import requests
 
-CSV_URL = 'https://archives.nseindia.com/products/content/sec_bhavdata_full_28072020.csv'
+CSV_URL = 'https://archives.nseindia.com/products/content/sec_bhavdata_full_02062020.csv'
+
+def getBhavdata():
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
+    
+        decoded_content = download.content.decode('utf-8')
+    
+        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+        my_list = list(cr)
+        for row in my_list:
+            print(row)
 
 
-with requests.Session() as s:
-    download = s.get(CSV_URL)
-
-    decoded_content = download.content.decode('utf-8')
-
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-    my_list = list(cr)
-    for row in my_list:
-        print(row)
+getBhavdata()
+print("=====================")
